8d94c3297ba4ef1fdc9cba0121f41aeb5b07f6b7
Armelle <armelle.lecerf@orange.fr>
2022-03-15 18:35:25 +0100

    Initial Commit

---
+++ PratiqueDeLaPoo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PratiqueDeLaPoo
+//
+//  Created by macmini-Armelle on 15/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
